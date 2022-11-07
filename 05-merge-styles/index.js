a67c0690fc9445b9c2cfae5dc57bbbeacb0824d0
Georgiy Beloklokov <georgiy.reus@gmail.com>
2022-11-07 17:13:41 +0300

    feat: 5

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,27 @@
+const { readdir } = require('node:fs/promises');
+const path = require('path');
+const fs = require('fs');
+
+async function bundleCss() {
+  const sourceFolder = path.join(__dirname, 'styles');
+  const content = await readdir(sourceFolder, { withFileTypes: true });
+  const files = content.filter(item => item.isFile());
+  
+  files.forEach(async item => {
+    const { ext } = path.parse(path.join(sourceFolder, item.name));
+   
+    if (ext === '.css') {
+      const readStream = fs.createReadStream(path.join(sourceFolder, item.name), 'utf-8');
+  
+      let data = '';
+      readStream.on('data', chunk => data += chunk);
+      readStream.on('end', () => fs.appendFile(
+        path.join(__dirname, 'project-dist', 'bundle.css'), data,
+        (err) => {
+            if (err) throw err;
+        }
+    ));
+    }
+    });
+};
+bundleCss().catch(`Error:`, console.error);
