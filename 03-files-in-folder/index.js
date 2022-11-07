5ac348f82c017ae42aeeb9bb3386635bcca72b2d
Georgiy Beloklokov <georgiy.reus@gmail.com>
2022-11-08 00:02:53 +0300

    fix: formatting

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,12 +1,14 @@
 const { readdir, stat } = require('fs/promises');
 const path = require('path');
 
-
 (async () => {
   try {
-    const content = await readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true });
-    const files = content.filter(item => item.isFile());
-    files.forEach(async item => {
+    const content = await readdir(path.join(__dirname, 'secret-folder'), {
+      withFileTypes: true,
+    });
+    const files = content.filter((item) => item.isFile());
+
+    files.forEach(async (item) => {
       const pathOfFile = path.join(path.join(__dirname, 'secret-folder'), item.name);
       const stats = await stat(pathOfFile);
       const { name, ext } = path.parse(pathOfFile);
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,17 +4,18 @@ const path = require('path');
 async function copyDir() {
   const projectFolder = path.join(__dirname, 'files-copy');
   const dirCreation = await mkdir(projectFolder, { recursive: true });
-  const content = await readdir(path.join(__dirname, 'files'), { withFileTypes: true });
-  const files = content.filter(item => item.isFile());
-    files.forEach(async item => {
-        const pathOfFile = path.join(path.join(__dirname, 'files'), item.name);
-        await copyFile(pathOfFile, path.join(path.join(__dirname, 'files-copy'), item.name));
-        console.log(`${item.name} was copied to files-copy`);
-    });
-    return dirCreation;
-};
+  const content = await readdir(path.join(__dirname, 'files'), {
+    withFileTypes: true,
+  });
+  const files = content.filter((item) => item.isFile());
+
+  files.forEach(async (item) => {
+    const pathOfFile = path.join(path.join(__dirname, 'files'), item.name);
+    await copyFile(pathOfFile, path.join(path.join(__dirname, 'files-copy'), item.name));
+    console.log(`${item.name} was copied to files-copy`);
+  });
+
+  return dirCreation;
+}
+
 copyDir().catch(`Error:`, console.error);
-
-
-
-  
